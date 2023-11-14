e50a358ba20ddcaa4d663926ac26b9027ce51646
Rahul <rahulkrish321123@gmail.com>
2023-11-14 22:06:35 +0530

    Navbar Done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {useState} from "react";
+import "./App.css"
+import {v4 as uuid} from "uuid"
+import Nav from "./Components/Nav.jsx";
+
+export default function App(){
+
+
+    return(
+        <div id={"main_container"}>
+            <Nav/>
+        </div>
+
+
+    )
+}
+
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,11 @@
+import "../Component_styles/Nav.css"
+
+
+export default function Nav(){
+    return(
+        <div id={"main__navbar"}>
+            <p>to</p>
+            <p id={"nav__purple-font"}>do.</p>
+        </div>
+    )
+}
